147c9b54fd20b8a7c027a50da1010ef2c74ef742
久治明千树汐 <admin@soraharu.com>
2025-04-20 12:14:48 +0800

    feat: using open-dkim

--- ServerMaintenance/Model/installNvidiaDriver.sh
+++ ServerMaintenance/Model/installNvidiaDriver.sh
@@ -1,8 +1,8 @@
 dnf config-manager --set-enabled crb
 dnf makecache
-dnf -y install epel-release
+dnf install -y epel-release
 dnf upgrade
 dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
 dnf makecache
-dnf module install nvidia-driver:latest
-dnf install freeglut-devel libX11-devel libXi-devel libXmu-devel make mesa-libGLU-devel freeimage-devel
+dnf module install nvidia-driver:open-dkim
+# dnf install freeglut-devel libX11-devel libXi-devel libXmu-devel make mesa-libGLU-devel freeimage-devel
