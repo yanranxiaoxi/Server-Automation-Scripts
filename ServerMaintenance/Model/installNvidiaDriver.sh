ab9b33297764b19f9fab4caea66971011c5b6ac1
久治明千树汐 <admin@soraharu.com>
2025-04-20 12:16:44 +0800

    chore: make prettier

--- ServerMaintenance/Model/installNvidiaDriver.sh
+++ ServerMaintenance/Model/installNvidiaDriver.sh
@@ -1,8 +1,8 @@
 dnf config-manager --set-enabled crb
-dnf makecache
-dnf install -y epel-release
+dnf install epel-release -y
 dnf upgrade
+
 dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo
-dnf makecache
-dnf module install nvidia-driver:open-dkim
+rpm --import https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/D42D0685.pub
+dnf module install nvidia-driver:open-dkim -y
 # dnf install freeglut-devel libX11-devel libXi-devel libXmu-devel make mesa-libGLU-devel freeimage-devel
