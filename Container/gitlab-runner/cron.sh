c98ac599d5069cebd3e75d24cfa3e74e72d94d31
XiaoXi <admin@soraharu.com>
2026-09-09 09:57:54 +0800

    fix: 修正清除历史镜像未成功的问题

--- Container/gitlab-runner/cron.sh
+++ Container/gitlab-runner/cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "40 2 * * * root podman system prune --all" >/etc/cron.d/container.gitlab-runner.cron
+echo "40 2 * * * root echo y | podman system prune --all" >/etc/cron.d/container.gitlab-runner.cron
 systemctl restart crond
